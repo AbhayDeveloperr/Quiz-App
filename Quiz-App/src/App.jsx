86080a2dccc1442910350b7a56c5b31bf8892e91
guruabhay25 <guptaabhay2727@gmail.com>
2026-07-02 20:06:40 +0530

    Initial of Quiz App

---
+++ Quiz-App/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      ABhay
+    </div>
+  )
+}
+
+export default App
---
+++ Quiz-App/src/data.js
@@ -0,0 +1,17 @@
+const data = [
+    {
+        question: "What is React?",
+        options: ["Library", "Framework", "Language", "Database"],
+        answer: "Library",
+    },
+    {
+        question: "Who developed React?",
+        options: ["Google", "Microsoft", "Meta", "Amazon"],
+        answer: "Meta",
+    },
+    {
+        question: "React uses ____ ?",
+        options: ["HTML", "CSS", "JSX", "XML"],
+        answer: "JSX",
+    }
+];
