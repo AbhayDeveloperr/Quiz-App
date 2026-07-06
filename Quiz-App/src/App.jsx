f5110add19b3bc488efa4305488ffe49fdd0813f
Abhay Kumar Gupta <guptaabhay2727@gmail.com>
2026-07-06 22:15:33 +0530

    Streak Maintains

--- Quiz-App/src/App.jsx
+++ Quiz-App/src/App.jsx
@@ -28,6 +28,7 @@ const App = () => {
         ))}
       </div>
       <button onClick={handleNext} className='bg-green-700 px-3 py-1 my-2 cursor-pointer active:scale-95'>Next</button>
+      console.log("Streak Maintaining")
     </div>
   )
 }
