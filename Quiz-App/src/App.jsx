4698c3dca10038594695966edd4b410ee3bf0907
guruabhay25 <guptaabhay2727@gmail.com>
2026-07-21 01:10:28 +0530

    Attempt Counter implemented

--- Quiz-App/src/App.jsx
+++ Quiz-App/src/App.jsx
@@ -14,15 +14,19 @@ const App = () => {
 
   const [maxAttempt, setMaxAttempt] = useState(false);
 
+  const [attemptCounter, setAttemptCounter] = useState(0);
+
   const handleNext=()=>{
     // Score Banana
     // console.log(questions.length)
     if(selectedOption===questions[currentQue].answer){
       setScore(score+1)
       
-    }
+    }            
+
+
     if(currentQue < questions.length-1){
-      setCurrentQue(currentQue+1)
+      setCurrentQue(currentQue+1)  //2
       setSelectedOption("")
       console.log(currentQue)
       
@@ -30,29 +34,42 @@ const App = () => {
       setIsFinished(true)
       setMaxAttempt(true)
     }
+    setAttemptCounter(attemptCounter+1)
+   
   };
   
-
+//check why the currentqueue value is not approaching to 3.
   return (
-    <div className='flex items-center justify-center flex-col pt-5'>
+    <div className='text-center pt-5'>
       <h1 className='text-3xl font-bold text-amber-300'>Quiz App</h1>
-
-      <h2 className='pt-8 font-semibold text-xl'>
+      <div id="game-over" className='mt-4'>
+        {isFinished && (
+          <p className="text-red-700 inline bg-red-300 px-4 py-2">
+            Game is over!! Your score is {score}
+          </p>
+        )}
+      </div>
+    <div className={` ${maxAttempt? 'hidden' : 'block'}`}>
+      <h2 className='pt-8 font-semibold text-xl text-center'>
         {questions[currentQue].question}
       </h2>
-      <div>
-        {questions[currentQue].options.map((option)=>(
-            <button onClick={()=>{
-              setSelectedOption(option)
-            }} className='block cursor-pointer'>{option}</button>
-        ))}
+      <div className=' flex justify-center items-center'>
+        <div>
+            {questions[currentQue].options.map((option)=>(
+              <button onClick={()=>{
+                setSelectedOption(option)
+              }} className='block cursor-pointer'>{option}</button>
+            ))}
+            <button onClick={handleNext} className={`bg-green-700 px-3 py-1 my-8 active:scale-95 ${maxAttempt? 'cursor-not-allowed' : 'cursor-pointer'}`}>{currentQue === questions.length - 1 ? "Submit" : "Next"}</button>
+        </div>
       </div>
-      <button onClick={handleNext} className={`bg-green-700 px-3 py-1 my-8 active:scale-95 ${maxAttempt? 'cursor-not-allowed' : 'cursor-pointer'}`}>{currentQue === questions.length - 1 ? "Submit" : "Next"}</button>
-
-      <p className='text-3xl mt-5'>
-        Your Score = {score}/{questions.length}
-      </p>
     </div>
+
+    <p className='text-3xl mt-5'>
+      
+       Total Attempts = {attemptCounter}
+    </p>
+  </div>
   )
 }
 
