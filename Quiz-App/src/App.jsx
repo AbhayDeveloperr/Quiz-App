3c7d546aa723865cf618d1149e70575d07b3df7a
guruabhay25 <guptaabhay2727@gmail.com>
2026-07-21 23:41:00 +0530

    UI improve

--- Quiz-App/src/App.jsx
+++ Quiz-App/src/App.jsx
@@ -39,36 +39,38 @@ const App = () => {
   
 //check why the currentqueue value is not approaching to 3.
   return (
-    <div className='text-center pt-8'>
-      <h1 className='text-5xl font-bold text-amber-300'>Quiz App</h1>
-      <div id="game-over" className='mt-4'>
-        {isFinished && (
-          <p className="text-red-700 inline bg-red-300 px-4 py-2">
-            Game is over!! Your score is {score}
-          </p>
-        )}
-      </div>
-    <div className={` ${maxAttempt? 'hidden' : 'block'}`}>
-      <h2 className='pt-8 pb-4 font-semibold text-2xl text-center'>
-        {questions[currentQue].question}
-      </h2>
-      <div className=' flex justify-center items-center'>
-        <div>
-            {questions[currentQue].options.map((option)=>(
-              <button onClick={()=>{
-                setSelectedOption(option)
-              }} className= {`block w-64 py-3 my-2 rounded-lg transition-all duration-300 hover:scale-105 active:scale-95 ${selectedOption==option? "bg-green-600 text-white" : "bg-gray-300 hover:bg-gray-400"}`}>{option}</button>
-            ))}
-            <button onClick={handleNext} className={`bg-sky-600 px-6 py-1 my-8 hover:scale-105 active:scale-95 ${maxAttempt? 'cursor-not-allowed' : 'cursor-pointer'}`}>{currentQue === questions.length - 1 ? "Submit" : "Next"}</button>
+    <div className='flex justify-center items-center h-screen'>
+      <div className='text-center pt-8 bg-red-900 shadow-lg shadow-gray-50 px-6 pb-8 rounded-xl h-[75vh]'>
+        <h1 className='text-5xl font-bold text-white'>Quiz App</h1>
+        <div id="game-over" className='mt-4'>
+          {isFinished && (
+            <p className="text-red-700 inline bg-red-300 px-4 py-2">
+              Game is over!! Your score is {score}
+            </p>
+          )}
+        </div>
+      <div className={` ${maxAttempt? 'hidden' : 'block'}`}>
+        <h2 className='pt-8 pb-4 font-semibold text-2xl text-center text-yellow-400'>
+          {questions[currentQue].question}
+        </h2>
+        <div className=' flex justify-center items-center'>
+          <div>
+              {questions[currentQue].options.map((option)=>(
+                <button onClick={()=>{
+                  setSelectedOption(option)
+                }} className= {`block w-64 py-3 my-2 rounded-lg transition-all duration-100 cursor-pointer hover:scale-105 active:scale-95 ${selectedOption==option? "bg-green-600 text-white" : "bg-gray-50 hover:bg-gray-100"}`}>{option}</button>
+              ))}
+              <button onClick={handleNext} className={`bg-black border-2 border-white text-white rounded-md px-12 py-2 my-8 hover:scale-105 active:scale-95 ${maxAttempt? 'cursor-not-allowed' : 'cursor-pointer'}`}>{currentQue === questions.length - 1 ? "Submit" : "Next"}</button>
+          </div>
         </div>
       </div>
-    </div>
 
-    <p className='text-3xl mt-5'>
-      
-       Total Attempts = {attemptCounter}
-    </p>
-  </div>
+      <p className='text-xl mt-5 text-gray-50 font-bold'>
+        
+        Total Attempts = {attemptCounter}
+      </p>
+      </div>
+    </div>
   )
 }
 
