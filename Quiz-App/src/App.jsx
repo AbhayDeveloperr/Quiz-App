7103d4a728aedec236ef8b2ca6acc98bb11c9bc2
guruabhay25 <guptaabhay2727@gmail.com>
2026-07-23 00:06:58 +0530

    Reset Button Implemented

--- Quiz-App/src/App.jsx
+++ Quiz-App/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
   const [attemptCounter, setAttemptCounter] = useState(0);
 
   const handleNext=()=>{
-
+    //Question Counter
     if (selectedOption === "") {
       alert("Please select an option first!");
       return;
@@ -40,6 +40,15 @@ const App = () => {
     setAttemptCounter(attemptCounter+1)
    
   };
+
+  const handleRestart=()=>{
+    setCurrentQue(0)
+    setSelectedOption("")
+    setScore(0)
+    setAttemptCounter(0)
+    setIsFinished(false)
+    setMaxAttempt(false)
+  }
   
 //check why the currentqueue value is not approaching to 3.
   return (
@@ -48,15 +57,23 @@ const App = () => {
         <h1 className='text-5xl font-bold text-white'>Quiz App</h1>
         <div id="game-over" className='mt-4'>
           {isFinished && (
-            <p className="text-red-700 inline bg-red-300 px-4 py-2">
-              Game is over!! Your score is {score}
-            </p>
+            <div className='mt-5'>
+              <p className="text-red-700 inline bg-red-300 px-4 py-2">
+                Game is over!! Your score is {score}
+              </p>
+              <br />
+              <button onClick={handleRestart} className="bg-green-600 text-white px-6 py-2 rounded mt-5 hover:bg-green-700">
+                Play Again
+              </button>
+            </div>
           )}
         </div>
       <div className={` ${maxAttempt? 'hidden' : 'block'}`}>
+        {/* Question Counter */}
         <p className='text-white text-lg font-semibold pt-2'>
           Question {currentQue+1}/{questions.length}
         </p>
+  
         <h2 className='pt-6 pb-4 font-semibold text-2xl text-center text-yellow-400'>
           {questions[currentQue].question}
         </h2>
